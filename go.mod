046fab431e65cfe74f36984718ee56f1ec747863
Pedro Brügger <pedromurilobrugger@hotmail.com>
2023-01-17 15:24:00 -0300

    Conectando API ao Postgres

---
+++ go.mod
@@ -0,0 +1,5 @@
+module API-IN-GOLANG
+
+go 1.19
+
+require github.com/lib/pq v1.10.7 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq" // O "_" significa que vai utilizar essa biblioteca apenas durante o tempo de execução da aplicação.
 )
 
+func connectDB() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=0506201Pb host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Produto struct {
+	Id              int
 	Nome, Descricao string
 	Preco           float64
 	Quantidade      int
@@ -14,18 +29,40 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html")) // Carrega todos os templates presentes no programa
 
 func main() {
+	fmt.Println("Conexão feita!")
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Macbook", "ARM 64", 12599, 1},
-		{"Pc Gamer", "AMD 5600G", 6000, 4},
+	db := connectDB()
+
+	selectAllProducts, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectAllProducts.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectAllProducts.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
