af1fe4458443f92bd78a4d64a7359c2199247273
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2023-01-17 09:48:15 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html")) // Carrega todos os templates presentes no programa
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Macbook", "ARM 64", 12599, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
