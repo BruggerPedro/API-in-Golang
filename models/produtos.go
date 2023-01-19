6fbc3012f8321a445ce5e229bf149083122152e3
Pedro Brügger <pedromurilobrugger@hotmail.com>
2023-01-19 15:25:21 -0300

    API concluída

--- models/produtos.go
+++ models/produtos.go
@@ -30,6 +30,7 @@ func GetAllProducts() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -53,3 +54,56 @@ func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	defer db.Close()
 }
 
+func DeletaProduto(id string) {
+	db := db.ConnectDB()
+
+	deletarOProduto, err := db.Prepare("DELETE FROM produtos WHERE id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarOProduto.Exec(id) // Executa o script de deletar produto
+	defer db.Close()
+}
+
+func EditaProduto(id string) Produto {
+	db := db.ConnectDB()
+
+	produtoDoBanco, err := db.Query("SELECT * FROM produtos WHERE id=$1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtoParaAtualizar := Produto{}
+
+	for produtoDoBanco.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = produtoDoBanco.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtoParaAtualizar.Id = id
+		produtoParaAtualizar.Nome = nome
+		produtoParaAtualizar.Descricao = descricao
+		produtoParaAtualizar.Preco = preco
+		produtoParaAtualizar.Quantidade = quantidade
+	}
+	defer db.Close()
+	return produtoParaAtualizar
+}
+
+func AtualizaProduto(id int, nome string, descricao string, preco float64, quantidade int) {
+	db := db.ConnectDB()
+
+	AtualizaProduto, err := db.Prepare("UPDATE produtos SET nome=$1, descricao=$2, preco=$3, quantidade=$4 WHERE id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+	AtualizaProduto.Exec(nome, descricao, preco, quantidade, id)
+
+	defer db.Close()
+}
